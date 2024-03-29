58f10269cf8fde4408af74ded7fea0b648eb3b2d
binary0ne <anton@procyon.ee>
2024-03-29 23:16:17 +0200

    Update setup.py
    imp to import lib

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 
 import os
 import sys
-import imp
+import importlib
 import subprocess
 import codecs
 
@@ -51,7 +51,7 @@ PYTEST_FLAGS = ['--doctest-modules']
 # instead, effectively side-stepping the dependency problem. Please make sure
 # metadata has no dependencies, otherwise they will need to be added to
 # the setup_requires keyword.
-metadata = imp.load_source(
+metadata = importlib.load_source(
     'metadata', os.path.join(CODE_DIRECTORY, 'metadata.py'))
 
 
